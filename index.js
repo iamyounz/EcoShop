66004e7f65042f80799d10f547f5b040a374073c
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-08-24 03:37:53 +0545

    ready to deploy first time

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express, { json } from "express";
-import connectDB from "./src/config/db.js";
 import dotenv from "dotenv";
-import authRoutes from "./src/routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -10,10 +8,11 @@ const app = express();
 app.use(json());
 
 // Routes
-app.use("/api/auth", authRoutes);
-
+app.get("/", (req, res) => {
+    res.json({
+        message: "Home page...",
+    });
+});
 app.listen(process.env.PORT, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}`);
-    // Database connection
-    connectDB();
 });
